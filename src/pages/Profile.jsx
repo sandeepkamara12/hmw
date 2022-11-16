e7a927f835a8c361614992eca8a7a1f547c284cb
webethics <info@webethicssolutions.com>
2022-11-16 19:48:25 +0530

    update the tabindex for profile page.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -110,9 +110,8 @@ const Profile = () => {
                backgroundColor: 'rgb(var(--color-fieldBg) / 1)!important',
                color: 'rgb(var(--color-placeholder)/1)',
                borderColor: 'rgb(var(--color-primary))!important',
-               borderRadius: '4px 4px 0 0!important',
-               borderBottomColor: 'rgb(var(--color-black)/1)!important',
-
+               borderRadius: provided.selectProps.menuIsOpen ? '4px 4px 0 0!important' : '4px!important',
+               borderBottomColor: provided.selectProps.menuIsOpen ? 'rgb(var(--color-black)/1)!important' : 'rgb(var(--color-fieldOutline)/1)',
             }
          }
       }),
@@ -159,26 +158,26 @@ const Profile = () => {
    return (
       <div className="custom-container text-center">
          <div className="header hidden sm:block">
-            <Link to="/" tabindex="1">
+            <Link to="/" tabIndex="1">
                <img src={Logo} alt="Logo" className="mx-auto mb-20 mt-9" />
             </Link>
          </div>
          <div className="custom-small-container mb-10 border-b-none">
             <h1 className='headingOne'>Profile</h1>
             <div className="form-control">
-               <label className="field-label text-left" tabindex="2">full name</label>
-               <input type="text" className="custom-input-field" placeholder="Add your name" autoFocus tabindex="3" />
+               <label className="field-label text-left" tabIndex="2">full name</label>
+               <input type="text" className="custom-input-field" placeholder="Add your name" autoFocus tabIndex="3" />
             </div>
             <div className="form-control">
-               <label className="field-label text-left" tabindex="4">Work email (Required for 2FA)
-                  {/* <Tooltip tabindex="5" /> */}
-                  <img tabindex="5" src={Info} alt="info icon" className='cursor-pointer inline-block ml-2' />
+               <label className="field-label text-left" tabIndex="4">Work email (Required for 2FA)
+                  {/* <Tooltip tabIndex="5" /> */}
+                  <img tabIndex="5" src={Info} alt="info icon" className='cursor-pointer inline-block ml-2' />
                </label>
-               <input type="text" className="custom-input-field" placeholder="Enter your work email" tabindex="6" />
+               <input type="text" className="custom-input-field" placeholder="Enter your work email" tabIndex="6" />
             </div>
             <div className="form-control">
-               <label className="field-label text-left" tabindex="7">Timezone<img tabindex="8" src={Info} alt="info icon" className='cursor-pointer inline-block ml-2' /></label>
-               <div className="select-wrapper" tabindex="9">
+               <label className="field-label text-left" tabIndex="7">Timezone<img tabIndex="8" src={Info} alt="info icon" className='cursor-pointer inline-block ml-2' /></label>
+               <div className="select-wrapper" tabIndex="9">
                   <TimezoneSelect
                      value={selectedTimezone}
                      onChange={setSelectedTimezone}
@@ -188,8 +187,8 @@ const Profile = () => {
                </div>
             </div>
             <div className="form-control">
-               <label className="field-label text-left" tabindex="10">Preferred language<img tabindex="11" src={Info} alt="info icon" className='cursor-pointer inline-block ml-2' /></label>
-               <div className="select-wrapper" tabindex="12">
+               <label className="field-label text-left" tabIndex="10">Preferred language<img tabIndex="11" src={Info} alt="info icon" className='cursor-pointer inline-block ml-2' /></label>
+               <div className="select-wrapper" tabIndex="12">
                   <Select
                      styles={customStyles}
                      placeholder="Select a language"
@@ -198,25 +197,25 @@ const Profile = () => {
                </div>
             </div>
             <div className="form-control">
-               <label className="field-label text-left" tabindex="13">My organization plans design initiatives by:<img tabindex="14" src={Info} alt="info icon" className='cursor-pointer inline-block ml-2' /></label>
+               <label className="field-label text-left" tabIndex="13">My organization plans design initiatives by:<img tabIndex="14" src={Info} alt="info icon" className='cursor-pointer inline-block ml-2' /></label>
                <ul className="grid gap-3 grid-cols-3 mb-7">
                   <li>
                      <input type="radio" id="quarter" name="hosting" value="quarter" className="hidden peer" required />
-                     <label htmlFor="quarter" tabindex="15" className="p-2.5 text-14 font-medium text-capitalize inline-block w-full text-fieldNoFocus rounded border border-fieldOutline cursor-pointer peer-checked:border-blue-600 peer-checked:text-primary hover:border-primary hover:text-primary">Quarter</label>
+                     <label htmlFor="quarter" tabIndex="15" className="p-2.5 text-14 font-medium text-capitalize inline-block w-full text-fieldNoFocus rounded border border-fieldOutline cursor-pointer peer-checked:border-blue-600 peer-checked:text-primary hover:border-primary hover:text-primary">Quarter</label>
                   </li>
                   <li>
                      <input type="radio" id="client" name="hosting" value="client" className="hidden peer" />
-                     <label htmlFor="client" tabindex="16" className="p-2.5 text-14 font-medium text-capitalize inline-block w-full text-fieldNoFocus rounded border border-fieldOutline cursor-pointer peer-checked:border-blue-600 peer-checked:text-primary hover:border-primary hover:text-primary">Client</label>
+                     <label htmlFor="client" tabIndex="16" className="p-2.5 text-14 font-medium text-capitalize inline-block w-full text-fieldNoFocus rounded border border-fieldOutline cursor-pointer peer-checked:border-blue-600 peer-checked:text-primary hover:border-primary hover:text-primary">Client</label>
                   </li>
                   <li>
                      <input type="radio" id="other" name="hosting" value="other" className="hidden peer" />
-                     <label htmlFor="other" tabindex="17" className="p-2.5 text-14 font-medium text-capitalize inline-block w-full text-fieldNoFocus rounded border border-fieldOutline cursor-pointer peer-checked:border-blue-600 peer-checked:text-primary hover:border-primary hover:text-primary">Other</label>
+                     <label htmlFor="other" tabIndex="17" className="p-2.5 text-14 font-medium text-capitalize inline-block w-full text-fieldNoFocus rounded border border-fieldOutline cursor-pointer peer-checked:border-blue-600 peer-checked:text-primary hover:border-primary hover:text-primary">Other</label>
                   </li>
                </ul>
             </div>
             <div className="form-control">
-               <label className="field-label text-left" tabindex="18">What month does Q1 begin each year?<img tabindex="19" src={Info} alt="info icon" className='cursor-pointer inline-block ml-2' /></label>
-               <div className="select-wrapper" tabindex="20">
+               <label className="field-label text-left" tabIndex="18">What month does Q1 begin each year?<img tabIndex="19" src={Info} alt="info icon" className='cursor-pointer inline-block ml-2' /></label>
+               <div className="select-wrapper" tabIndex="20">
                   <Select
                      styles={customStyles}
                      placeholder="Select a month"
@@ -225,8 +224,8 @@ const Profile = () => {
                </div>
             </div>
             <div className="form-control">
-               <label className="field-label text-left" tabindex="21">How many design initiatives do you complete each quarter?</label>
-               <div className="select-wrapper" tabindex="22">
+               <label className="field-label text-left" tabIndex="21">How many design initiatives do you complete each quarter?</label>
+               <div className="select-wrapper" tabIndex="22">
                   <Select
                      styles={customStyles}
                      placeholder="Select an option"
@@ -234,7 +233,7 @@ const Profile = () => {
                   />
                </div>
             </div>
-            <Button tabindex="23" classes='custom-button custom-button-large custom-button-fill-primary' attributes={{ disabled: true, value: "Submit", clickEvent: handleClick }} />
+            <Button tabIndex="23" classes='custom-button custom-button-large custom-button-fill-primary' attributes={{ disabled: true, value: "Submit", clickEvent: handleClick }} />
          </div>
       </div >
    )
