f093a7d8d3f4089f1c81e8ed1ce2a298af684df0
webethics <info@webethicssolutions.com>
2022-11-23 11:07:11 +0530

    update few design changes.

--- src/components/formElements/Button.jsx
+++ src/components/formElements/Button.jsx
@@ -13,7 +13,7 @@ const Button = (props) => {
                <path d="M50 88 A38 38 0 0 1 12 50.00000000000001" fill="none" stroke="#ffffff" strokeWidth="3"></path>
                <path d="M12 50.00000000000001 A38 38 0 0 1 49.99999999999999 12" fill="none" stroke="#ffffff" strokeWidth="3"></path>
                <path d="M49.99999999999999 12 A38 38 0 0 1 88 49.99999999999999" fill="none" stroke="#000000" strokeWidth="3"></path>
-               <animateTransform attributeName="transform" type="rotate" values="0 50 50;360 50 50" times="0;1" dur="6.666666666666666s" repeatCount="indefinite"></animateTransform>
+               <animateTransform attributeName="transform" type="rotate" values="0 50 50;360 50 50" times="0;1" dur="1s" repeatCount="indefinite"></animateTransform>
             </g>
          </svg>
          <span className={`${props.attributes.loader ? 'hidden' : ''}`}>{t(props.attributes.value)}</span>
