95f23a4e851e63474b4c1d2946a6e9f1846b6cac
detailswes <details.wes@gmail.com>
2022-11-28 16:58:32 +0530

    sign Error Fixed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import './App.css';
-import routes from './routes';
-import { Routes, Route,useNavigate } from 'react-router-dom';
-import { useState,useEffect } from 'react';
+import "./App.css";
+import routes from "./routes";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import "./i18n";
 import { useTranslation } from "react-i18next";
 import ProtectedRoutes from "./components/ProtectedRoute";
-import SigninEmail from './pages/SigninEmail';
-import SigninPhone from './pages/SigninPhone';
-import VerificationWithPhone from './pages/VerificationWithPhone';
-import VerificationWithEmail from './pages/VerificationWithEmail';
-import { Navigate } from 'react-router-dom';
-import { userActions } from './store/slices/userSlice';
-
+import SigninEmail from "./pages/SigninEmail";
+import SigninPhone from "./pages/SigninPhone";
+import VerificationWithPhone from "./pages/VerificationWithPhone";
+import VerificationWithEmail from "./pages/VerificationWithEmail";
+import { Navigate } from "react-router-dom";
+import { userActions } from "./store/slices/userSlice";
 
 const languages = [
-  { value: 'en', text: "English" },
-  { value: 'ch', text: "Swedish" },
-]
-
+  { value: "en", text: "English" },
+  { value: "ch", text: "Swedish" },
+];
 
 function App() {
-
   const { t, i18n } = useTranslation();
   const [selectedLng, setSelectedLng] = useState(i18n.language);
   const dispatch = useDispatch();
@@ -29,21 +26,21 @@ function App() {
 
   // This function put query that helps to
   // change the language
-  const handleChange = e => {
-    const lng =e.target.value;
+  const handleChange = (e) => {
+    const lng = e.target.value;
     i18n.changeLanguage(lng);
     localStorage.setItem("lang", lng);
     setSelectedLng(lng);
-  }
+  };
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     // const userInfo = localStorage.getItem("user");
     if (token) {
-        dispatch(userActions.userLoggedIn(true));
-        navigate("/profile-setup");
+      dispatch(userActions.userLoggedIn(true));
+      // navigate("/profile-setup");
     }
-}, [dispatch,navigate]);
+  }, [dispatch, navigate]);
   return (
     <div className="App">
       {/* <select value={selectedLng} onChange={handleChange}>
@@ -52,22 +49,18 @@ function App() {
                     value={item.value}>{item.text}</option>);
                 })}
             </select> */}
-            <Routes>
-                <Route path="/" element={<Navigate to="/auth" />} />
-                <Route path="/auth" element={<SigninPhone />} />
-                <Route path="/auth/email" element={<SigninEmail />} />
-                <Route path="/auth/verify" element={<VerificationWithPhone />} />
-                <Route path="/auth/verify/email" element={<VerificationWithEmail />} />
-                <Route element={<ProtectedRoutes />}>
-                    {routes.map((res, index) => (
-                        <Route
-                            path={res.path}
-                            element={res.component}
-                            key={index}
-                        />
-                    ))}
-                </Route>
-            </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/auth" />} />
+        <Route path="/auth" element={<SigninPhone />} />
+        <Route path="/auth/email" element={<SigninEmail />} />
+        <Route path="/auth/verify" element={<VerificationWithPhone />} />
+        <Route path="/auth/verify/email" element={<VerificationWithEmail />} />
+        <Route element={<ProtectedRoutes />}>
+          {routes.map((res, index) => (
+            <Route path={res.path} element={res.component} key={index} />
+          ))}
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/pages/SigninPhone.jsx
+++ src/pages/SigninPhone.jsx
@@ -1,97 +1,138 @@
-import React, { useState } from 'react';
-import Button from '../components/formElements/Button';
-import { Link, useNavigate } from 'react-router-dom';
-import PhoneInput from 'react-phone-input-2'
-import 'react-phone-input-2/lib/style.css'
+import React, { useState } from "react";
+import Button from "../components/formElements/Button";
+import { Link, useNavigate } from "react-router-dom";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 import startsWith from "lodash/startsWith";
 import authService from "./../services/authService";
 
 const SigninPhone = () => {
-   const [phone, setPhone] = useState("");
-   const [showLoader, setShowLoader] = useState(false);
-   const [validationError, setValidationError] = useState();
-   const navigate = useNavigate();
-   const [phoneNumberIsValid, setPhoneNumberIsValid] = useState(false);
+  const [phone, setPhone] = useState("");
+  const [showLoader, setShowLoader] = useState(false);
+  const [validationError, setValidationError] = useState();
+  const navigate = useNavigate();
+  const [phoneNumberIsValid, setPhoneNumberIsValid] = useState(false);
 
-   const verificationCodeHandler = async () => {
-      setShowLoader(true);
-      setValidationError(null);
-      try {
-         let finalPhoneNumber = '';
-         if (phone) {
-            finalPhoneNumber = `+${phone}`;
-         }
-         await authService.requestVerificationCodeByPhone(finalPhoneNumber);
-         localStorage.setItem('phoneNumber', finalPhoneNumber);
-         navigate("/auth/verify");
-         setShowLoader(false);
-      } catch (error) {
-         const { statusCode, message } = error.response.data;
-         if (statusCode === 422 || statusCode === 400) {
-            if(Array.isArray(message)) {
-               setValidationError(message[0]);
-            } else {
-               setValidationError(message);
-            }
-         }
-         setShowLoader(false);
-         setPhoneNumberIsValid(true)
+  const verificationCodeHandler = async () => {
+    setShowLoader(true);
+    setValidationError(null);
+    try {
+      let finalPhoneNumber = "";
+      if (phone) {
+        finalPhoneNumber = `+${phone}`;
       }
-   }
- 
-   return (
-      <div className="custom-container text-center">
-         <div className="header hidden sm:block">
-            <Link to="/" className='mx-auto mb-20 mt-9 inline-block'>
-               <svg xmlns="http://www.w3.org/2000/svg" width="62" height="15" viewBox="0 0 62 15" fill="none">
-                  <path fillRule="evenodd" clipRule="evenodd" d="M28.2304 10.6421L23.2193 0L16.1406 14.4376H26.3695H30.0176H40.2464L33.4481 0L28.2304 10.6421Z" fill="#FE7A48" />
-                  <path fillRule="evenodd" clipRule="evenodd" d="M50.7648 0.982422H40.252V9.7431C40.252 12.6461 42.6053 14.9995 45.5084 14.9995C48.4114 14.9995 50.7648 12.6461 50.7648 9.7431V0.982422ZM61.2793 0.982422H50.7664V9.7431C50.7664 12.6461 53.1198 14.9995 56.0229 14.9995C58.9259 14.9995 61.2793 12.6461 61.2793 9.7431V0.982422Z" fill="#044FF5" />
-                  <path fillRule="evenodd" clipRule="evenodd" d="M0.72168 1.11987H6.74903V4.48188H9.13035V1.11987H15.1577V14.4361H9.13035V11.0699H6.74903V14.4361H0.72168V1.11987Z" fill="#FECD48" />
-               </svg>
-            </Link>
-         </div>
-         <div className="custom-small-container">
-            <h1 className='headingOne'>Sign in</h1>
-            <div className="form-control">
-               <label className="field-label text-left">phone number</label>
-               <PhoneInput
-                  country={'us'}
-                  value={phone}
-                  onChange={phone => setPhone(phone)}
-                  containerClass="mb-7"
-                  // searchClass="country-code-search"
-                  enableSearch
-                  searchPlaceholder="Search"
-                  autocompleteSearch={true}
-                  disableSearchIcon
-                  inputClass={`custom-input-field ${phoneNumberIsValid ? 'border !border-red-500' : '!bg-white'}`}
-                  inputProps={{
-                     name: 'phone',
-                     required: true,
-                     // autoFocus: true,
-                     placeholder: 'Phone number',
-                  }}
-                  onBlur={(e, country) => {
-                     let _length = country.format.split(".").length - 1;
-                     // startsWith(phone, country.dialCode) || startsWith(country.dialCode, phone);
-                     if (!phone.length || phone.length !== _length) {
-                           setPhoneNumberIsValid(true);
-                     }
-                 }}
-                 onFocus={(e) => {setPhoneNumberIsValid(false)}}
-               />
-               {validationError?.length &&
-                  <span className='field-label-error field-error field-label'>{validationError}</span>
-               }
-            </div>
-            <Button classes='custom-button custom-button-large custom-button-fill-primary' attributes={{ type: 'submit', disabled: false, value: "Request verification code", clickEvent: verificationCodeHandler, loader: showLoader }} />
-         </div>
-         <div className="custom-small-container border-none py-0">
-            <Link to="/auth/email" className="textLink mb-11">Use an email instead</Link>
-            <p className='content'>By selecting ‘Request verification code’ you agree to our <Link to="#" className='normalLink'>Terms of Service</Link>.</p>
-         </div>
+      await authService.requestVerificationCodeByPhone(finalPhoneNumber);
+      localStorage.setItem("phoneNumber", finalPhoneNumber);
+      navigate("/auth/verify");
+      setShowLoader(false);
+    } catch (error) {
+      const { statusCode, message } = error.response.data;
+      if (statusCode === 422 || statusCode === 400) {
+        if (Array.isArray(message)) {
+          setValidationError(message[0]);
+        } else {
+          setValidationError(message);
+        }
+      }
+      setShowLoader(false);
+      setPhoneNumberIsValid(true);
+    }
+  };
+
+  return (
+    <div className="custom-container text-center">
+      <div className="header hidden sm:block">
+        <Link to="/" className="mx-auto mb-20 mt-9 inline-block">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="62"
+            height="15"
+            viewBox="0 0 62 15"
+            fill="none"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M28.2304 10.6421L23.2193 0L16.1406 14.4376H26.3695H30.0176H40.2464L33.4481 0L28.2304 10.6421Z"
+              fill="#FE7A48"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M50.7648 0.982422H40.252V9.7431C40.252 12.6461 42.6053 14.9995 45.5084 14.9995C48.4114 14.9995 50.7648 12.6461 50.7648 9.7431V0.982422ZM61.2793 0.982422H50.7664V9.7431C50.7664 12.6461 53.1198 14.9995 56.0229 14.9995C58.9259 14.9995 61.2793 12.6461 61.2793 9.7431V0.982422Z"
+              fill="#044FF5"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M0.72168 1.11987H6.74903V4.48188H9.13035V1.11987H15.1577V14.4361H9.13035V11.0699H6.74903V14.4361H0.72168V1.11987Z"
+              fill="#FECD48"
+            />
+          </svg>
+        </Link>
       </div>
-   )
-}
+      <div className="custom-small-container">
+        <h1 className="headingOne">Sign in</h1>
+        <div className="form-control">
+          <label className="field-label text-left">phone number</label>
+          <PhoneInput
+            country={"us"}
+            value={phone}
+            onChange={(phone) => setPhone(phone)}
+            // searchClass="country-code-search"
+            enableSearch
+            searchPlaceholder="Search"
+            autocompleteSearch={true}
+            disableSearchIcon
+            inputClass={`custom-input-field ${
+              phoneNumberIsValid ? "border !border-red-500" : "!bg-white"
+            }`}
+            inputProps={{
+              name: "phone",
+              required: true,
+              // autoFocus: true,
+              placeholder: "Phone number",
+            }}
+            onBlur={(e, country) => {
+              let _length = country.format.split(".").length - 1;
+              // startsWith(phone, country.dialCode) || startsWith(country.dialCode, phone);
+              if (!phone.length || phone.length !== _length) {
+                setPhoneNumberIsValid(true);
+              }
+            }}
+            onFocus={(e) => {
+              setPhoneNumberIsValid(false);
+            }}
+          />
+          {validationError?.length && (
+            <div className="field-label-error text-left">{validationError}</div>
+          )}
+        </div>
+        <Button
+          classes="custom-button custom-button-large custom-button-fill-primary"
+          attributes={{
+            type: "submit",
+            disabled: false,
+            value: "Request verification code",
+            clickEvent: verificationCodeHandler,
+            loader: showLoader,
+          }}
+        />
+      </div>
+      <div className="custom-small-container border-none py-0">
+        <Link to="/auth/email" className="textLink mb-11">
+          Use an email instead
+        </Link>
+        <p className="content">
+          By selecting ‘Request verification code’ you agree to our{" "}
+          <Link to="#" className="normalLink">
+            Terms of Service
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default SigninPhone;
