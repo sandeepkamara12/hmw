b09a4123e420598db72cb65b9be0a8dea20d87ca
webethics <info@webethicssolutions.com>
2022-12-02 13:16:48 +0530

    Updated shirt size, tracks, quarter dropdown options

---
+++ src/utils/constants.js
@@ -0,0 +1,20 @@
+export const QUARTERS = [
+  { value: "Q1", label: "Q1" },
+  { value: "Q2", label: "Q2" },
+  { value: "Q3", label: "Q3" },
+  { value: "Q4", label: "Q4" },
+];
+
+export const TRACKS = [
+  { value: "Digital Acquisition", label: "Digital Acquisition" },
+  { value: "Engagement", label: "Engagement" },
+  { value: "Retention", label: "Retention" },
+];
+
+export const TSHIRT_SIZES = [
+  { value: "XS (<1 week) ", label: "XS (<1 week)" },
+  { value: "S (1-2 weeks)", label: "S (1-2 weeks)" },
+  { value: "M (2-4 weeks)", label: "M (2-4 weeks)" },
+  { value: "L (4-6 weeks)", label: "L (4-6 weeks)" },
+  { value: "XL (6+ weeks)", label: "XL (6+ weeks)" },
+];
